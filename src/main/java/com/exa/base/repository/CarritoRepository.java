8df3d38cf3f9ec21a85a777cc21cbbc3c1cd9e28
Cesar886 <cesar4herrera@gmail.com>
2025-04-28 15:25:45 -0500

    Hola

--- src/main/java/com/exa/base/repository/CarritoRepository.java
+++ src/main/java/com/exa/base/repository/CarritoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CarritoRepository extends JpaRepository<Carrito, Long> {
-    Optional<Carrito> findByUsuarioId(Long usuarioId);
+    Optional<Carrito> findByUsuarioId(Long usuarioId); // ¡Método correcto!
 }
